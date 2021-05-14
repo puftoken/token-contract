400cdad5c094fd4474a6ee7c9e6ebdd806c78302
PUF Token <84174503+puftoken@users.noreply.github.com>
2021-05-15 02:16:40 +0300

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -17,6 +17,6 @@ contract Token is ERC20, ERC20Detailed {
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
     constructor () public ERC20Detailed("PUFToken", "PUFT", 8) {
-        _mint(msg.sender, 21000000 * (10 ** uint256(decimals())));
+        _mint(msg.sender, 21000000);
     }
 }
